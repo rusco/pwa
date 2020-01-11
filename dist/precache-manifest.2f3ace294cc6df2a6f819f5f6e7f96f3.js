0b161a9194436fae929664f6c041162462047af8
Rusco <rusco81@gmail.com>
2020-01-11 16:35:19 +0000

    Add missing files

---
+++ dist/precache-manifest.2f3ace294cc6df2a6f819f5f6e7f96f3.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a5171be38e0df7bf48",
+    "url": "/css/app.b55ef383.css"
+  },
+  {
+    "revision": "5637785d35cc0e3a456b",
+    "url": "/css/chunk-vendors.905b5e3e.css"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "1618c77b6c5c11926819a8868a083031",
+    "url": "/fonts/materialdesignicons-webfont.1618c77b.ttf"
+  },
+  {
+    "revision": "927457ed7478ec7e1580a2b88116f9c3",
+    "url": "/fonts/materialdesignicons-webfont.927457ed.woff2"
+  },
+  {
+    "revision": "d6e3eba9b16453f51d4ef6e216bf89ff",
+    "url": "/fonts/materialdesignicons-webfont.d6e3eba9.woff"
+  },
+  {
+    "revision": "f81583fc4d8aa5aa2820976561ea8aec",
+    "url": "/fonts/materialdesignicons-webfont.f81583fc.eot"
+  },
+  {
+    "revision": "19e1c02211e01b11c4b8b8084e281983",
+    "url": "/img/Amkor_Technology_logo.svg"
+  },
+  {
+    "revision": "a06bbf5565ccce42572124f8dbf425e6",
+    "url": "/img/Amkor_Technology_logo.svg.png"
+  },
+  {
+    "revision": "7654af4d35fac50742a524279229991d",
+    "url": "/img/amkor-logo-blue.jpg"
+  },
+  {
+    "revision": "c3b6e5faef7b42304b17db4a66050258",
+    "url": "/img/amkor-logo.jpg"
+  },
+  {
+    "revision": "19ea9346a5a6c2d3aa1f8bea435971fb",
+    "url": "/img/amkor.jfif"
+  },
+  {
+    "revision": "6b73c60530c4e131d5460017c8e635b0",
+    "url": "/img/amkor200.200.png"
+  },
+  {
+    "revision": "16bf253c7ea26f51cfd78e02ced9b309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a5171be38e0df7bf48",
+    "url": "/js/app.f5cbd305.js"
+  },
+  {
+    "revision": "5637785d35cc0e3a456b",
+    "url": "/js/chunk-vendors.31900b12.js"
+  },
+  {
+    "revision": "4bef6960a19d9c165a06512ee225b8bd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
